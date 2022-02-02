6fcdca1cf8a454bd899c181ceb4571ad12f8cc0a
Aleksandr Galkin <thebest-earnings@yandex.ru>
2022-02-02 10:08:05 +0200

    first commit

---
+++ js/common.js
@@ -0,0 +1,25 @@
+$(function() {
+
+	$('.slider').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		arrows: true,
+		prevArrow: `<button type="button" class="slick-prev">
+		<svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M0.441406 11L9.4414 21.0125L11.5589 18.6568L4.67035 10.9933L11.5589 3.3299L9.4414 0.987549L0.441406 11Z" fill="#D6DBE1"/>
+		</svg>		
+		</button>`,
+		nextArrow: `<button type="button" class="slick-next">
+		<svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M0.441406 11L9.4414 21.0125L11.5589 18.6568L4.67035 10.9933L11.5589 3.3299L9.4414 0.987549L0.441406 11Z" fill="#D6DBE1"/>
+		</svg>
+		</button>`,
+	});
+
+	$('.menu-toggler').on("click", function(e) {
+		$('body').toggleClass("menu-openned");
+	});
+
+});
